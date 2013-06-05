822ef1f71c563b37965470f34f68566eb7759161
chrizdee <cd@novo-online.de>
2013-06-05 14:26:43 +0200

    Added english translation

--- TL_ROOT/modules/formgenerator_avisota/languages/en/default.php
+++ TL_ROOT/modules/formgenerator_avisota/languages/en/default.php
@@ -1,6 +1,6 @@
 <?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
 
 $GLOBALS['TL_LANG']['formgenerator_avisota']['subject'] = 'Newsletter subscription';
-$GLOBALS['TL_LANG']['formgenerator_avisota']['message'] = 'Sie sind bereits bei folgenden Newsletter-Listen angemeldet: ';
+$GLOBALS['TL_LANG']['formgenerator_avisota']['message'] = 'You already signed up the following lists: ';
 
 ?>
